b818dda8633c2681cd163763092569c763dbb3a3
Stas Kazachok <kazachokstas@gmail.com>
2017-08-01 01:03:12 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <windows.h>
+#include "MainMenu.h"
+#include "ExitMenu.h"
+#include "Game.h"
+
+HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+BOOL ShowConsoleCursor(BOOL bShow);
+
+void drawField();
+
+int main() {
+	ShowConsoleCursor(FALSE);
+
+	MainMenu* mainMenu = new MainMenu;
+	ExitMenu* exitMenu = new ExitMenu(33, 10);
+	Game* game;
+
+	int mainMenuChoise;
+	int exitMenuChoise = 0;
+
+	do {
+		mainMenuChoise = mainMenu->show();
+
+		switch (mainMenuChoise) {
+			case 0:
+				game = new Game;
+				game->draw();
+				game->start();
+				delete game;
+			break;
+			case 1:
+				exitMenuChoise = exitMenu->show();
+			break;
+		}
+	}
+	while (exitMenuChoise != 1);
+}
+
+BOOL ShowConsoleCursor(BOOL bShow)
+{
+    CONSOLE_CURSOR_INFO cci;
+
+    if(hStdOut == INVALID_HANDLE_VALUE)
+        return FALSE;
+
+    if(!GetConsoleCursorInfo(hStdOut, &cci))
+        return FALSE;
+
+    cci.bVisible = bShow;
+
+    if(!SetConsoleCursorInfo(hStdOut,&cci))
+        return FALSE;
+
+    return TRUE;
+}
